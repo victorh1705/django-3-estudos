f974100eaeb4df950456ea885850e79c48b54769
victor henrique <victorh1705@gmail.com>
2020-08-23 02:25:52 -0300

    adding admin options to domain 'escola'

--- alura-django-3-api-rest/escola/admin.py
+++ alura-django-3-api-rest/escola/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
 # Register your models here.
+from escola.models import Aluno, Curso
+
+
+class Alunos(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'rg', 'cpf', 'data_nascimento')
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome',)
+    list_per_page = 20
+
+
+admin.site.register(Aluno, Alunos)
+
+
+class Cursos(admin.ModelAdmin):
+    list_display = ('id', 'codigo_curso', 'descricao')
+    list_display_links = ('id', 'descricao')
+    search_fields = ('codigo_curso',)
+    list_per_page = 20
+
+
+admin.site.register(Curso, Cursos)
--- alura-django-3-api-rest/escola/models.py
+++ alura-django-3-api-rest/escola/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+
 # Create your models here.
+class Aluno(models.Model):
+    nome = models.CharField(max_length=50)
+    rg = models.CharField(max_length=11)
+    cpf = models.CharField(max_length=9)
+    data_nascimento = models.DateField()
+
+    def __str__(self):
+        return self.nome
+
+
+class Curso(models.Model):
+    NIVEL = (
+        ('B', 'Básico'),
+        ('I', 'Intermediário'),
+        ('A', 'Avançado'),
+    )
+    codigo_curso = models.CharField(max_length=10)
+    descricao = models.CharField(max_length=100)
+    nivel = models.CharField(max_length=1, choices=NIVEL, default='B', null=False, blank=False)
+
+    def __str__(self):
+        return self.descricao
