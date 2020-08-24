0601b100215b43867a699ddd39d5b834d09be3de
victor henrique <victorh1705@gmail.com>
2020-08-23 23:50:52 -0300

    adding serializers and viewsets

--- alura-django-3-api-rest/setup/urls.py
+++ alura-django-3-api-rest/setup/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
-from escola.views import alunos
+from escola.views import AlunosViewSet, CursosViewSet
+
+router = routers.DefaultRouter()
+router.register('alunos', AlunosViewSet, basename='Alunos')
+router.register('cursos', CursosViewSet, basename='Cursos')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('alunos/', alunos),
+    path('', include(router.urls)),
 ]
