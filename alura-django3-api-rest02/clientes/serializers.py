8e9f21b9d4627a939577aae40503985e1b09f57c
victor henrique <victorh1705@gmail.com>
2020-09-15 16:10:27 -0300

    adding validators

--- alura-django3-api-rest02/clientes/serializers.py
+++ alura-django3-api-rest02/clientes/serializers.py
@@ -1,8 +1,22 @@
 from rest_framework import serializers
+
 from clientes.models import Cliente
+from . import validators
+
 
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = '__all__'
-    
+
+    def validate(self, data):
+        print(data)
+        if not validators.cpf_valido(data['cpf']):
+            raise serializers.ValidationError({'cpf': "O CPF deve conter 11 dígitos"})
+        if not validators.nome_valido(data['nome']):
+            raise serializers.ValidationError({'nome': "Não inclua números no nome"})
+        if not validators.rg_valido(data['rg']):
+            raise serializers.ValidationError({'rg': "O RG deve conter 9 dígitos"})
+        if not validators.celular_valido(data['celular']):
+            raise serializers.ValidationError({'celular': "O CELULAR deve conter 11 dígitos"})
+        return data
---
+++ alura-django3-api-rest02/readme.md
@@ -0,0 +1,15 @@
+# Estudos alura
+
+
+#### Super user
+
+- nome: victor.henrique
+- email: victor@gmail.com
+- password: admin
+
+### Postgres
+
+- user: postgres
+- password: mysecretpassword 
+
+
